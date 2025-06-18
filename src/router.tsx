e0594886f710e640e73b8610127e35e6600a7dd5
wobsoriano <sorianorobertc@gmail.com>
2025-06-18 13:33:29 -0700

    chore: introduce new structure

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,8 @@ export function createRouter() {
   const router = createTanStackRouter({
     routeTree,
     defaultPreload: 'intent',
+    defaultErrorComponent: (err) => <p>{err.error.stack}</p>,
+    defaultNotFoundComponent: () => <p>not found</p>,
     scrollRestoration: true,
   })
 
